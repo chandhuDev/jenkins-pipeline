7441334bd454822c116b4ffc1a5b737ed786ec3b
chandhu <chandhudev0@gmail.com>
2024-04-02 22:26:50 +0530

    update: updates the both the jenkins files

--- File-Paramater/commands.sh
+++ File-Paramater/commands.sh
@@ -1,8 +1,9 @@
-yum -y update
+yum install -y update
 yum install  -y wget
+wget --version
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-yum -y update
+yum install -y update
 
 yum install -y fontconfig java-17-openjdk
 yum install -y jenkins
@@ -11,12 +12,10 @@ systemctl daemon-reload
 echo java -version
 
 systemctl enable jenkins
-systemctl start jenkins
 systemctl status jenkins
 
 yum install -y firewalld
 systemctl start firewalld
-systemctl status firewalld
 systemctl start jenkins
 firewall-cmd --zone=public --add-port=8080/tcp --permanent
 firewall-cmd --reload
